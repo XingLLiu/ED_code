85e753b8f0924ba2769cb188ddbd2688e8ba2d4b
edrysdale <erik.drysdale@sickkids.ca>
2019-07-24 09:37:48 -0400

    updating the clinical notes processing

--- ClinicalNotePreProcessing/clean_extract_notes.sh
+++ ClinicalNotePreProcessing/clean_extract_notes.sh
@@ -1,11 +1,33 @@
 #!/bin/bash
 
-cd /
-cd path/to/ED/ED_code
-python cleaning_script.py
-cd /
-cd path/to/apache-ctakes-4.0.0/bin
-/bin/bash path/to/apache-ctakes-4.0.0/bin/runClinicalPipeline  -i path/to/ED/ED_code/annotatorInput  --xmiOut path/to/ED/ED_code/annotatorOutput   --user UMLSUser  --pass UMLSpassword
-cd /
-cd path/to/ED/ED_code
-python extract_CUIs.py
+dir_apache=${1:-/usr/local/apache-ctakes-4.0.0/bin}
+
+# Maria's user/pass
+uname="MariaY"
+passwd="Frat3llis!"
+
+echo "----- Apache folder: "$dir_apache" -----"
+# Check that runClinicalPipeline exists
+apache_hit=$(ls $dir_apache | grep runClinicalPipeline.sh | wc -m)
+if [ $apache_hit -lt 5 ]; then
+	echo "Error! runClinicalPipeline.sh was not found in dir_apache"
+fi
+
+# Get the current diectory 
+dir=$(pwd)
+# Folder that should have the EPIC.csv file in it
+dir_EPIC=$dir/../../data/EPIC_DATA
+epic_hit=$(ls $dir_EPIC | grep ^EPIC.csv$ | wc -m)
+if [ $epic_hit -lt 5 ]; then
+	echo "Error! EPIC.csv was not found two folders up in ~data/"
+	return
+fi
+
+echo "----- Running python cleaning script -----"
+python $dir/cleaning_script.py --path $dir_EPIC --local $dir
+echo "----- Changing folder to apache-ctakes -----"
+cd $dir_apache
+. $dir_apache/runClinicalPipeline.sh  -i $dir_EPIC/annotatorInput  --xmiOut $dir_EPIC/annotatorOutput   --user $uname  --pass $passwd
+echo "----- Running python CUI extractor script -----"
+cd $dir
+python $dir/extract_CUIs.py --path $dir_EPIC
