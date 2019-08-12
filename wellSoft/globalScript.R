123c0b29a4c5eb46c0e272a669b70060b76a2980
leborade <lebo.radebe@hotmail.com>
2019-08-12 14:13:45 -0400

    added different file processing

--- wellSoft/globalScript.R
+++ wellSoft/globalScript.R
@@ -5,6 +5,7 @@ source('processRegistrationCodes.R')
 source('cleanWellSoft.R')
 source('preprocessWellSoft.R')
 source('timeLapse.R')
+source('calculateReturnVisits.R')
 
 data.path <- "/home/lebo/mybigdata/data/"
 
@@ -18,7 +19,7 @@ library(scales)
 library(bizdays)
 library(openxlsx)
 library(stringr)
-
+library(zoo)
 
 
 
@@ -30,13 +31,14 @@ library(stringr)
 #     - I. Read all raw data
 #     - II. Preprocess Registration Codes (processRegistrationCodes.R)
 #     - III. Clean wellSoft Data
-#   B. Load cleaned Data 
 #
 # 2. CALCULATE OTHER STATS FROM CLEANED WELLSOFT
 #   A. Time lapse 
 #   B. Return visits
 #
-# 3. PREPROCESS WELLSOFT DATA
+# 3. LOAD CLEANED DATA
+#
+# 4. PREPROCESS WELLSOFT DATA
 
 
 # ==================  1. PREPROCESS DATA  ==================
@@ -52,12 +54,6 @@ reg_codes <- processRegistrationCodes(reg_codes, data.path)
 #   - III. Clean wellSoft Data 
 wellSoft <- cleanWellSoft(wellSoft, data.path)
 
-##  ==========    OR   ==========
-
-# B. Load Preprocessed Data 
-wellSoft <- fread(paste0(data.path, "cleaned_wellSoft.csv"))
-reg_codes <- fread(paste0(data.path, "processedRegistrationCodes.csv"))
-
 
 
 # ================== 2. CALCULATE OTHER STATS FROM CLEANED WELLSOFT  ================== # 
@@ -65,6 +61,21 @@ reg_codes <- fread(paste0(data.path, "processedRegistrationCodes.csv"))
 # A. timeLapse.csv --> input cleaned_wellSoft.csv and processedRegistrationCodes.csv
 timeLapse <- calculateTimeLapse(wellSoft, reg_codes, data.path)
 
+
+# B. willReturn.csv --> input all_data.csv and timeLapse.csv
+diff.days <- 3.00 # sets the difference between days (3.00 = 72 h)
+all_data <- fread(paste0(data.path, "all_data.csv"))
+
+willReturn <- calculateReturnVisits(all_data, timeLapse, diff.days, data.path)
+
+
+##  ==========    3. (OR) LOAD CLEANED DATA   ==========
+
+wellSoft <- fread(paste0(data.path, "cleaned_wellSoft.csv"))
+reg_codes <- fread(paste0(data.path, "processedRegistrationCodes.csv"))
+all_data <- fread(paste0(data.path, "all_data.csv"))
+timeLapse <- fread(paste0(data.path, "timeLapse.csv"))
+willReturn <- fread(paste0(data.path, "willReturn.csv"))
 
 
 # ================== 3. PREPROCESS WELLSOFT DATA  ================== # 
