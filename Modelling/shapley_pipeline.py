557b052c9069ea89c3d60d7e23ac60e6029d3aa1
XingLLiu <liuxing971015@outlook.com>
2019-08-22 17:14:31 -0400

    [22/08/2019]

--- Modelling/shapley_pipeline.py
+++ Modelling/shapley_pipeline.py
@@ -255,84 +255,84 @@ _ = evaluator.roc_plot(plot = True, title = MODEL_NAME)
 
 
 
-# ----------------------------------------------------
-# ========= 2.b. Built-in Shapley =========
-import os
-import sys
-import time
-import numpy as np
-import matplotlib.pyplot as plt
-import sklearn
-from ED_support_module.Shapley import ShapNN
-from ED_support_module.DShap import DShap
-from ED_support_module.shap_utils import *
+# # ----------------------------------------------------
+# # ========= 2.b. Built-in Shapley =========
+# import os
+# import sys
+# import time
+# import numpy as np
+# import matplotlib.pyplot as plt
+# import sklearn
+# from ED_support_module.Shapley import ShapNN
+# from ED_support_module.DShap import DShap
+# from ED_support_module.shap_utils import *
 
-problem, model = 'classification', 'logistic'
-hidden_units = [] # Empty list in the case of logistic regression.
-train_size = 100
-
-
-d, difficulty = 50, 1
-num_classes = 2
-tol = 0.03
-target_accuracy = 0.7
-important_dims = 5
-clf = return_model(model, solver='liblinear', hidden_units=tuple(hidden_units))
-_param = 1.0
-for _ in range(100):
-    X_raw = np.random.multivariate_normal(mean=np.zeros(d), cov = np.eye(d), 
-                                          size=train_size + 5000)
-    _, y_raw, _, _ = label_generator(
-        problem, X_raw, param = _param,  difficulty = difficulty, important=important_dims)
-    clf.fit(X_raw[:train_size], y_raw[:train_size])
-    test_acc = clf.score(X_raw[train_size:], y_raw[train_size:])
-    if test_acc>target_accuracy:
-        break
-    _param *= 1.1
+# problem, model = 'classification', 'logistic'
+# hidden_units = [] # Empty list in the case of logistic regression.
+# train_size = 100
 
 
-print('Performance using the whole training set = {0:.2f}'.format(test_acc))
+# d, difficulty = 50, 1
+# num_classes = 2
+# tol = 0.03
+# target_accuracy = 0.7
+# important_dims = 5
+# clf = return_model(model, solver='liblinear', hidden_units=tuple(hidden_units))
+# _param = 1.0
+# for _ in range(100):
+#     X_raw = np.random.multivariate_normal(mean=np.zeros(d), cov = np.eye(d), 
+#                                           size=train_size + 5000)
+#     _, y_raw, _, _ = label_generator(
+#         problem, X_raw, param = _param,  difficulty = difficulty, important=important_dims)
+#     clf.fit(X_raw[:train_size], y_raw[:train_size])
+#     test_acc = clf.score(X_raw[train_size:], y_raw[train_size:])
+#     if test_acc>target_accuracy:
+#         break
+#     _param *= 1.1
 
 
-# Runing
-X, y = X_raw[:train_size], y_raw[:train_size]
-X_test, y_test = X_raw[train_size:], y_raw[train_size:]
-model = 'logistic'
-problem = 'classification'
-num_test = 1000
-directory = './temp'
-sources = None
-dshap = DShap(X, y, X_test, y_test, num_test, sources=sources, model_family=model, metric='accuracy',
-              directory=directory, seed=0)
-dshap.run(100, 0.1)
-
-X, y = X_raw[:100], y_raw[:100]
-X_test, y_test = X_raw[100:], y_raw[100:]
-model = 'logistic'
-problem = 'classification'
-num_test = 1000
-directory = './temp'
-dshap = DShap(X, y, X_test, y_test, num_test, model_family=model, metric='accuracy',
-              directory=directory, seed=1)
-dshap.run(100, 0.1)
-
-X, y = X_raw[:100], y_raw[:100]
-X_test, y_test = X_raw[100:], y_raw[100:]
-model = 'logistic'
-problem = 'classification'
-num_test = 1000
-directory = './temp'
-dshap = DShap(X, y, X_test, y_test, num_test, model_family=model, metric='accuracy',
-              directory=directory, seed=2)
-dshap.run(100, 0.1)
+# print('Performance using the whole training set = {0:.2f}'.format(test_acc))
 
 
-# Merge results
-dshap.merge_results()
+# # Runing
+# X, y = X_raw[:train_size], y_raw[:train_size]
+# X_test, y_test = X_raw[train_size:], y_raw[train_size:]
+# model = 'logistic'
+# problem = 'classification'
+# num_test = 1000
+# directory = './temp'
+# sources = None
+# dshap = DShap(X, y, X_test, y_test, num_test, sources=sources, model_family=model, metric='accuracy',
+#               directory=directory, seed=0)
+# dshap.run(100, 0.1)
 
-convergence_plots(dshap.marginals_tmc)
+# X, y = X_raw[:100], y_raw[:100]
+# X_test, y_test = X_raw[100:], y_raw[100:]
+# model = 'logistic'
+# problem = 'classification'
+# num_test = 1000
+# directory = './temp'
+# dshap = DShap(X, y, X_test, y_test, num_test, model_family=model, metric='accuracy',
+#               directory=directory, seed=1)
+# dshap.run(100, 0.1)
 
-convergence_plots(dshap.marginals_g)
+# X, y = X_raw[:100], y_raw[:100]
+# X_test, y_test = X_raw[100:], y_raw[100:]
+# model = 'logistic'
+# problem = 'classification'
+# num_test = 1000
+# directory = './temp'
+# dshap = DShap(X, y, X_test, y_test, num_test, model_family=model, metric='accuracy',
+#               directory=directory, seed=2)
+# dshap.run(100, 0.1)
 
-dshap.performance_plots([dshap.vals_tmc, dshap.vals_g, dshap.vals_loo], num_plot_markers=20,
-                       sources=dshap.sources)
+
+# # Merge results
+# dshap.merge_results()
+
+# convergence_plots(dshap.marginals_tmc)
+
+# convergence_plots(dshap.marginals_g)
+
+# dshap.performance_plots([dshap.vals_tmc, dshap.vals_g, dshap.vals_loo], num_plot_markers=20,
+#                        sources=dshap.sources)
